4cf75b3f1b1735a0f732400f6f66e07ff4e0e3c8
dhana-01 <tmdbyatigammana@gmail.com>
2025-09-03 04:48:22 +0530

    feat(fe): Build static components (Header, Footer, BookCard)

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header-container">
+        <div className="logo">
+          <Link to="/" className="logo-link">
+            Bookstore
+          </Link>
+        </div>
+        <nav className="navigation">
+          <ul className="nav-list">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/store" className="nav-link">
+                Store
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact" className="nav-link">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
